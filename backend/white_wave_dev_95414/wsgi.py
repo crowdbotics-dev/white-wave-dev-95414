afe14777e43e32639c8e0133cb6ffa95d569f886
Crowdbotics <engineering@crowdbotics.com>
2023-06-28 09:01:37 +0000

    Initial commit.

---
+++ backend/white_wave_dev_95414/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for white_wave_dev_95414 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'white_wave_dev_95414.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "white-wave-dev-95414",
+  url: "https://white-wave-dev-95414.botics.co",
+  api: "https://white-wave-dev-95414.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
